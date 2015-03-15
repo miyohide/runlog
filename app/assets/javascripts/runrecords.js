105fcdfee9b2c3ea661e5a77ec78b2ffc44d8dbf
miyohide <miyohide@gmail.com>
2015-03-15 22:04:16 +0900

    show runlog bar graph

--- app/assets/javascripts/runrecords.js
+++ app/assets/javascripts/runrecords.js
@@ -1,8 +1,23 @@
 $(document).ready(function() {
-  var plot_data = [[]];
+  var plot_data = [];
   for(var i = 0; i < gon.runrecords.length; i++ ) {
-    plot_data[0].push(gon.runrecords[i].distance);
+    plot_data.push([gon.runrecords[i].runned_at, gon.runrecords[i].distance]);
   }
-  $.jqplot("chartdiv", plot_data);
+  $.jqplot("chartdiv", [plot_data], {
+    title: "Run Log",
+    series:[{renderer:$.jqplot.BarRenderer}],
+    axesDefaults: {
+      tickRenderer: $.jqplot.CanvasAxisTickRenderer ,
+      tickOptions: {
+        angle: -30,
+        fontSize: '10pt'
+      }
+    },
+    axes: {
+      xaxis: {
+        renderer: $.jqplot.CategoryAxisRenderer
+      }
+    }
+  });
 });
 
