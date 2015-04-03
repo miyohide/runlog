12785cf021028f88979c54ed83ae4583696ac582
miyohide <miyohide@gmail.com>
2015-04-03 22:45:22 +0900

    Implement distance/runned_at

--- app/models/garmin_log.rb
+++ app/models/garmin_log.rb
@@ -5,8 +5,19 @@ class GarminLog < ActiveRecord::Base
     csv_file.write if csv_file.from?
   end
 
+  after_save :parse_csv
+
   after_destroy do
     csv_file.delete
+  end
+
+  def parse_csv
+    parse_result = []
+    CSV.parse(self.csv_file.read) do |row|
+      next if row[0].strip != "true" && row[0].strip != "false"
+      parse_result << { distance: row[6].to_f , run_time: row[5] }
+    end
+    parse_result
   end
 end
 
