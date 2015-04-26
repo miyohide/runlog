8a1a737deeefd8323ed709d5b1d5217e3437d6e0
miyohide <miyohide@gmail.com>
2015-04-26 19:11:15 +0900

    modify user_sessions routes. add login/logout routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-
-  get 'user_sessions/create'
-
-  get 'user_sessions/destroy'
+  root "users#index"
 
   resources :users
+  resources :user_sessions
 
   resources :runrecords, except: [:destroy]
   resources :garmin_logs, only: [:create]
 
-  root "runrecords#index"
+  get 'login' => 'user_sessions#new', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
 
 end
