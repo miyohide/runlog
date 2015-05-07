085704ce4b4588533f717b75668341b9d816d49d
miyohide <miyohide@gmail.com>
2015-05-07 22:39:27 +0900

    add user utility methods.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,19 @@ class User < ActiveRecord::Base
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
   validates :email, uniqueness: true
+
+  belongs_to :role
+
+  def ability
+    Ability.ability_hash self.role.roles_abilities.map(&:ability_id)
+  end
+
+  def admin?
+    ability.include? "admin"
+  end
+
+  scope :except_admin, -> {
+    joins(:role).where.not(roles: { name: "administrator"} )
+  }
 end
 
