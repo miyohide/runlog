e55daa5646bf045d423c3dde347f0a580264c54e
miyohide <miyohide@gmail.com>
2015-05-11 21:59:01 +0900

    add pundit methods.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,18 @@ class ApplicationController < ActionController::Base
 
   before_filter :require_login
 
+  rescue_from Pundit::NotAuthorizedError, with: :render_404
+
   private
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
+  end
+
+  def render_404(exception = nil)
+    if exception
+      logger.info "Rendering 404 with exception: #{exception.message}"
+    end
+
+    render file: "#{Rails.root}/public/404.html", status: 404, content_type: "text/html"
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :pundit_auth
   skip_before_filter :require_login, only: [:index, :new, :create]
 
   def index
@@ -48,5 +49,13 @@ class UsersController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:email, :password, :password_confirmation, :salt)
+    end
+
+    def pundit_auth
+      if current_user
+        authorize(current_user)
+      else
+        authorize(User.new)
+      end
     end
 end
