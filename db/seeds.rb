77f35c33ea4fdab4ea9fa06fa4b98e3a74aa68a8
miyohide <miyohide@gmail.com>
2015-05-24 21:53:57 +0900

    use factory_girls

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require "factory_girl"
 
 YAML.load_file(Rails.root.join("db", "seeds", "nike_run_logs.yml")).each do |run_log|
   Runrecord.create(runned_at: run_log[:start_time],
                    distance:  run_log[:distance].to_f.round(3),
                    run_time:  run_log[:duration][0..-5])  # 最後のコンマ秒数は削除
 end
+
+Dir[Rails.root.join("spec", "factories", "*.rb")].each { |f| require f }
+
+Ability.delete_all
+Role.delete_all
+RolesAbility.delete_all
+
+FactoryGirl.create(:admin_role)
+FactoryGirl.create(:user_role)
+FactoryGirl.create(:runrecord_role)
 
