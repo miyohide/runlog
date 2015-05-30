2681d009cab703b7926501ac43add2c30d252eff
miyohide <miyohide@gmail.com>
2015-05-31 00:14:50 +0900

    delete unused role

--- spec/support/role_contexts.rb
+++ spec/support/role_contexts.rb
@@ -2,11 +2,7 @@ shared_context "管理者" do
   let(:user) { FactoryGirl.create(:administrator) }
 end
 
-shared_context "User" do
-  let(:user) { FactoryGirl.create(:has_user_authority) }
-end
-
-shared_context "Role" do
-  let(:user) { FactoryGirl.create(:has_role_authority) }
+shared_context "Runrecord" do
+  let(:user) { FactoryGirl.create(:has_runrecord_authority) }
 end
 
