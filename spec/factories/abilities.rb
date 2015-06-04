08fe36faa92c46f2daba75eaad10c003cba48396
miyohide <miyohide@gmail.com>
2015-06-04 23:31:21 +0900

    delete role ability & fix runrecord ability

--- spec/factories/abilities.rb
+++ spec/factories/abilities.rb
@@ -30,52 +30,27 @@ FactoryGirl.define do
   end
 
   factory :runrecord_index, class: Ability do
-    domain "role"
+    domain "runrecord"
     ability "index"
   end
 
   factory :runrecord_show, class: Ability do
-    domain "role"
+    domain "runrecord"
     ability "show"
   end
 
   factory :runrecord_create, class: Ability do
-    domain "role"
+    domain "runrecord"
     ability "create"
   end
 
   factory :runrecord_update, class: Ability do
-    domain "role"
+    domain "runrecord"
     ability "update"
   end
 
   factory :runrecord_destroy, class: Ability do
-    domain "role"
-    ability "destroy"
-  end
-
-  factory :role_index, class: Ability do
-    domain "role"
-    ability "index"
-  end
-
-  factory :role_show, class: Ability do
-    domain "role"
-    ability "show"
-  end
-
-  factory :role_create, class: Ability do
-    domain "role"
-    ability "create"
-  end
-
-  factory :role_update, class: Ability do
-    domain "role"
-    ability "update"
-  end
-
-  factory :role_destroy, class: Ability do
-    domain "role"
+    domain "runrecord"
     ability "destroy"
   end
 
