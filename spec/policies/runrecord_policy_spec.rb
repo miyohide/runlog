461f394e8139fda4a3a9d0fc25bf77e8d008dda6
miyohide <miyohide@gmail.com>
2015-06-05 20:24:25 +0900

    Use shared examples

--- spec/policies/runrecord_policy_spec.rb
+++ spec/policies/runrecord_policy_spec.rb
@@ -5,86 +5,30 @@ describe RunrecordPolicy do
   subject { described_class }
 
   permissions :index? do
-    context "登録した人" do
-      include_context "Runrecord"
-      it { should permit(user, Runrecord.new(user_id: user.id)) }
-    end
-
-    context "登録した人以外" do
-      include_context "Runrecord"
-      it { should_not permit(user, Runrecord.new) }
-    end
+    it_behaves_like "Runrecord examples"
   end
 
   permissions :show? do
-    context "登録した人" do
-      include_context "Runrecord"
-      it { should permit(user, Runrecord.new(user_id: user.id)) }
-    end
-
-    context "登録した人以外" do
-      include_context "Runrecord"
-      it { should_not permit(user, Runrecord.new) }
-    end
+    it_behaves_like "Runrecord examples"
   end
 
   permissions :new? do
-    context "登録した人" do
-      include_context "Runrecord"
-      it { should permit(user, Runrecord.new(user_id: user.id)) }
-    end
-
-    context "登録した人以外" do
-      include_context "Runrecord"
-      it { should_not permit(user, Runrecord.new) }
-    end
+    it_behaves_like "Runrecord examples"
   end
 
   permissions :create? do
-    context "登録した人" do
-      include_context "Runrecord"
-      it { should permit(user, Runrecord.new(user_id: user.id)) }
-    end
-
-    context "登録した人以外" do
-      include_context "Runrecord"
-      it { should_not permit(user, Runrecord.new) }
-    end
+    it_behaves_like "Runrecord examples"
   end
 
   permissions :update? do
-    context "登録した人" do
-      include_context "Runrecord"
-      it { should permit(user, Runrecord.new(user_id: user.id)) }
-    end
-
-    context "登録した人以外" do
-      include_context "Runrecord"
-      it { should_not permit(user, Runrecord.new) }
-    end
+    it_behaves_like "Runrecord examples"
   end
 
   permissions :edit? do
-    context "登録した人" do
-      include_context "Runrecord"
-      it { should permit(user, Runrecord.new(user_id: user.id)) }
-    end
-
-    context "登録した人以外" do
-      include_context "Runrecord"
-      it { should_not permit(user, Runrecord.new) }
-    end
+    it_behaves_like "Runrecord examples"
   end
 
   permissions :destroy? do
-    context "登録した人" do
-      include_context "Runrecord"
-      it { should permit(user, Runrecord.new(user_id: user.id)) }
-    end
-
-    context "登録した人以外" do
-      include_context "Runrecord"
-      it { should_not permit(user, Runrecord.new) }
-    end
+    it_behaves_like "Runrecord examples"
   end
 end
