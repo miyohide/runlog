187d2e2d9ff1d4476e3d619f19fecc11c7287bb4
miyohide <miyohide@gmail.com>
2015-06-05 20:12:34 +0900

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503112958) do
+ActiveRecord::Schema.define(version: 20150605104714) do
 
   create_table "abilities", force: true do |t|
     t.string   "domain"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150503112958) do
     t.string   "run_time"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
