47a08b893d80eaff00269e1423f90d57826c5fcc
miyohide <miyohide@gmail.com>
2015-06-23 22:23:47 +0900

    refactoring.

--- app/helpers/runrecords_helper.rb
+++ app/helpers/runrecords_helper.rb
@@ -1,11 +1,23 @@
 module RunrecordsHelper
   def distance_per_month_table(user)
-    rval = "<table><thead><tr><th>日付</th><th>距離</th></tr></thead><tbody>"
-    Runrecord.distance_summary_per_month(user).each do |distances|
-      rval += "<tr><td>#{distances[:date]}</td><td>#{"%.2f" % distances[:total_distance]}</td></tr>"
+    # create thead area.
+    thead = content_tag :thead do
+      content_tag :tr do
+        concat content_tag(:th, "日付")
+        concat content_tag(:th, "距離")
+      end
     end
-    rval += "</tbody></table>"
 
-    rval.html_safe
+    # create tbody area.
+    tbody = content_tag :tbody do
+      Runrecord.distance_summary_per_month(user).map {|distances|
+        content_tag :tr do
+          concat content_tag(:td, distances[:date])
+          concat content_tag(:td, "%.2f" % distances[:total_distance])
+        end
+      }.join().html_safe
+    end
+
+    content_tag :table, thead.concat(tbody)
   end
 end
