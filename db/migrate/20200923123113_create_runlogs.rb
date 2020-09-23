3d8218e05fe43b78b9a7328d8b72cd0bd95dc39c
Hidenori Miyoshi <miyohide@gmail.com>
2020-09-23 21:33:34 +0900

    add null is false option.

--- db/migrate/20200923123113_create_runlogs.rb
+++ db/migrate/20200923123113_create_runlogs.rb
@@ -1,8 +1,8 @@
 class CreateRunlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :runlogs do |t|
-      t.datetime :run_date
-      t.decimal :distance
+      t.datetime :run_date, null: false
+      t.decimal :distance, null: false
 
       t.timestamps
     end
