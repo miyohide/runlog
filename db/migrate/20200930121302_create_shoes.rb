63ff325db3f77e0a794cf40e2a5ed327ce69e401
Hidenori Miyoshi <miyohide@gmail.com>
2020-09-30 21:31:25 +0900

    add null and unique restrict.

--- db/migrate/20200930121302_create_shoes.rb
+++ db/migrate/20200930121302_create_shoes.rb
@@ -1,9 +1,10 @@
 class CreateShoes < ActiveRecord::Migration[6.0]
   def change
     create_table :shoes do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
+    add_index :shoes, :name, unique: true
   end
 end
