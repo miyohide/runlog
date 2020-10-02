63e1e38959eebda4581b2ebaea7bd1251936fd7d
Hidenori Miyoshi <miyohide@gmail.com>
2020-10-02 22:16:32 +0900

    fix typo.

--- db/migrate/20200930125058_add_references_to_runlogs.rb
+++ db/migrate/20200930125058_add_references_to_runlogs.rb
@@ -1,5 +1,5 @@
 class AddReferencesToRunlogs < ActiveRecord::Migration[6.0]
   def change
-    add_reference :runglos, :shoe, foreign_key: true
+    add_reference :runlogs, :shoe, foreign_key: true
   end
 end
