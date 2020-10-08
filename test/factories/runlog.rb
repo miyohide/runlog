538702205d4e5b35c9a7a10cbb16977f862e97dd
Hidenori Miyoshi <miyohide@gmail.com>
2020-10-08 20:46:46 +0900

    Fix FactoryBot define of runlog.

--- test/factories/runlog.rb
+++ test/factories/runlog.rb
@@ -2,6 +2,12 @@ FactoryBot.define do
   factory :runlog do
     run_date { Time.now }
     distance { 10.0 }
-    total_time { 3663 }
+    total_time_hours { 1 }
+    total_time_minutes { 2 }
+    total_time_seconds { 3 }
+
+    trait :with_shoe do
+      association :shoe
+    end
   end
 end
