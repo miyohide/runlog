d6d1afdf184a80b4e1f6bb1de6ace8270da4ed38
Hidenori Miyoshi <miyohide@gmail.com>
2020-10-14 21:35:14 +0900

    Fix fail test.

--- test/models/runlog_test.rb
+++ test/models/runlog_test.rb
@@ -2,12 +2,14 @@ require 'test_helper'
 
 class RunlogTest < ActiveSupport::TestCase
   test "calculate total_time" do
+    shoe = FactoryBot.create(:shoe)
     runlog = Runlog.create(
       run_date: Time.now,
       distance: 1.0,
       total_time_hours: 1,
       total_time_minutes: 2,
-      total_time_seconds: 3
+      total_time_seconds: 3,
+      shoe: shoe
     )
     assert_equal 3_723, runlog.total_time
   end
