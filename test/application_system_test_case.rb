91f74878d5e43368b4e51aab9f6a4cdf854db488
Hidenori Miyoshi <miyohide@gmail.com>
2020-10-31 18:32:26 +0900

    config for system test.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,14 @@
 require "test_helper"
 
+Capybara.server_host = Socket.ip_address_list.detect{ |addr| addr.ipv4_private? }.ip_address
+Capybara.server_port = 3002
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_chrome, options: {
+    browser: :remote,
+    url: ENV.fetch('SELENIUM_REMOTE_URL'),
+    desired_capabilities: :chrome
+  }
+  host! "http://#{Capybara.server_host}:#{Capybara.server_port}"
+  WebMock.allow_net_connect!
 end
