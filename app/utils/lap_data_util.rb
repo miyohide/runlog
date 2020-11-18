8c361fa12b1f32e835c5b4ef13ac71457ec1131c
Hidenori Miyoshi <miyohide@gmail.com>
2020-11-18 21:08:11 +0900

    ヘッダが日本語だとうまく動かないので列の番号にした

--- app/utils/lap_data_util.rb
+++ app/utils/lap_data_util.rb
@@ -4,7 +4,7 @@ class LapDataUtil
   def self.CSV2LapData(contents)
     rval = []
     CSV.parse(contents, headers: true).each do |row|
-      rval << {distance: row['距離'].to_f, elapsedtime: TimeStr2Seconds(row['タイム'])}
+      rval << {distance: row[3].to_f, elapsedtime: TimeStr2Seconds(row[1])}
     end
     rval
   end
