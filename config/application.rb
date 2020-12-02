cec3a547a3103270cc6596395b08cec66fafc9ce
Hidenori Miyoshi <miyohide@gmail.com>
2020-12-02 20:39:36 +0900

    add field_error_proc

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,17 @@ module Myapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    ## エラーがある入力項目に対してuk-form-dangerクラスを追加して目立たせる
+    ## ための処理を行う
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      if html_tag =~ /<(input|textarea|select)/
+        p html_tag
+        html_field = Nokogiri::HTML::DocumentFragment.parse(html_tag)
+        html_field.children.add_class 'uk-form-danger'
+        html_field.to_s.html_safe
+      else
+        html_tag
+      end
+    end
   end
 end
