50aa9a350a7caf3276148e3db454ab3418ff0990
Hidenori Miyoshi <miyohide@gmail.com>
2021-06-09 20:49:49 +0900

    add null validation

--- db/migrate/20210608113145_create_runrecords.rb
+++ db/migrate/20210608113145_create_runrecords.rb
@@ -1,9 +1,9 @@
 class CreateRunrecords < ActiveRecord::Migration[6.1]
   def change
     create_table :runrecords do |t|
-      t.timestamp :starttime
-      t.float :distance
-      t.integer :runtime
+      t.timestamp :starttime, null: false
+      t.float :distance, null: false
+      t.integer :runtime, null: false
 
       t.timestamps
     end
