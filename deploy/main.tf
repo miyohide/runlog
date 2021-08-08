8d028632ef9d01a9cbd16512e7432d595ceae2d5
Hidenori Miyoshi <miyohide@gmail.com>
2021-08-08 20:40:31 +0900

    fix typo

--- deploy/main.tf
+++ deploy/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "rg" {
   location = "japaneast"
 }
 
-# Create an Azure Container Registory
+# Create an Azure Container Registry
 resource "azurerm_container_registry" "acr" {
   name                     = "crmiyohiderailsapp"
   resource_group_name      = azurerm_resource_group.rg.name
