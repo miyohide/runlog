4edd117ceab24c44ff32607c41559fc8b7457589
Hidenori Miyoshi <miyohide@gmail.com>
2021-08-19 21:43:02 +0900

    add postgresql variables

--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,3 +1,26 @@
 variable "rails_master_key" {
   type = string
 }
+
+variable "postgresql-admin-login" {
+  type = string
+}
+
+variable "postgresql-admin-password" {
+  type = string
+}
+
+variable "postgresql-version" {
+  type = string
+  default = "11"
+}
+
+variable "postgresql-sku-name" {
+  type = string
+  default = "B_Gen5_1"
+}
+
+variable "postgresql-storage" {
+  type = string
+  default = "5120"
+}
