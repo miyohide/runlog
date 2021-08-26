4442590ab361326593445c1f59305a00b2f21a54
Hidenori Miyoshi <miyohide@gmail.com>
2021-08-26 21:05:11 +0900

    add distance validation

--- app/models/runrecord.rb
+++ app/models/runrecord.rb
@@ -1,2 +1,5 @@
 class Runrecord < ApplicationRecord
+  # Validationの設定
+  # see. https://railsguides.jp/active_record_validations.html
+  validates :distance, numericality: { greater_than: 0.0}
 end
--- test/models/runrecord_test.rb
+++ test/models/runrecord_test.rb
@@ -5,4 +5,14 @@ class RunrecordTest < ActiveSupport::TestCase
     runrecord = Runrecord.new(starttime: Time.now, distance: 10.0, runtime: 600)
     assert runrecord.valid?
   end
+
+  test "距離が負の数の時、Runrecordは妥当ではない" do
+    runrecord = Runrecord.new(starttime: Time.now, distance: -1.0, runtime: 600)
+    assert_not runrecord.valid?
+  end
+
+  test "距離が0の時、Runrecordは妥当ではない" do
+    runrecord = Runrecord.new(starttime: Time.now, distance: 0.0, runtime: 600)
+    assert_not runrecord.valid?
+  end
 end
