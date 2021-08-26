291a54baa1458806d9738c5444fec4a53f112b4f
Hidenori Miyoshi <miyohide@gmail.com>
2021-08-26 21:00:28 +0900

    create valid runrecord test.

--- test/models/runrecord_test.rb
+++ test/models/runrecord_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class RunrecordTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "適切な値を入れたとき、Runrecordが作られる" do
+    runrecord = Runrecord.new(starttime: Time.now, distance: 10.0, runtime: 600)
+    assert runrecord.valid?
+  end
 end
