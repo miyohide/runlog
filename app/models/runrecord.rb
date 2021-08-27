95565fb730d7a3d2e9000fabed6bb7453749441e
Hidenori Miyoshi <miyohide@gmail.com>
2021-08-27 20:20:02 +0900

    add validation for runtime

--- app/models/runrecord.rb
+++ app/models/runrecord.rb
@@ -2,4 +2,5 @@ class Runrecord < ApplicationRecord
   # Validationの設定
   # see. https://railsguides.jp/active_record_validations.html
   validates :distance, numericality: { greater_than: 0.0}
+  validates :runtime, numericality: { only_integer: true, greater_than: 0 }
 end
