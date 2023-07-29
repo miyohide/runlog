7f414caebf3ba47a5af19a66cf41de7629470062
Hidenori Miyoshi <miyohide@gmail.com>
2023-07-29 20:55:16 +0900

    get data from rails api server

--- frontend/src/displayGraph.ts
+++ frontend/src/displayGraph.ts
@@ -1,22 +1,40 @@
 import Chart from "chart.js/auto";
 
+const apiURL = 'http://localhost:3000/';
+
+const getData = async () => {
+  const response = await fetch(`${apiURL}`);
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return Promise.reject(response.status);
+  }
+}
+
 export function displayGraph(element: HTMLCanvasElement) {
-  new Chart(element, {
-    type: 'bar',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 4, 2,3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+  const result = getData();
+
+  result.then((data) => {
+    new Chart(element, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: data,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
         }
       }
-    }
-  });
+    });
+  })
+  .catch((error) => {
+    console.log('Error: ' + error);
+  })
 }
