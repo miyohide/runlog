ab107ebbeadb7b3b110f562a0702985ffdc3c9e5
Hidenori Miyoshi <miyohide@gmail.com>
2023-07-29 18:43:10 +0900

    display graph

---
+++ frontend/src/displayGraph.ts
@@ -0,0 +1,22 @@
+import Chart from "chart.js/auto";
+
+export function displayGraph(element: HTMLCanvasElement) {
+  new Chart(element, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 4, 2,3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,13 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
+import { displayGraph } from './displayGraph.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div>
+      <canvas id="myChart"></canvas>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+displayGraph(document.querySelector<HTMLCanvasElement>('#myChart')!)
