f393dffb74eec63ab005a73bf7860d67ad425422
Hidenori Miyoshi <miyohide@gmail.com>
2023-08-17 16:18:43 +0900

    add order by running_date

--- api/app/graphql/types/query_type.rb
+++ api/app/graphql/types/query_type.rb
@@ -8,7 +8,7 @@ module Types
     # They will be entry points for queries on your schema.
     field :runlogs, [Types::RunlogType], null: false
     def runlogs
-      Runlog.all
+      Runlog.order(running_date: :asc).all
     end
 
     # TODO: remove me
