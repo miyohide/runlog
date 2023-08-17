bc8bbe7306431fe9c11582b9062fb486e7df17d4
Hidenori Miyoshi <miyohide@gmail.com>
2023-08-17 16:01:26 +0900

    fix captions

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,8 +3,8 @@ import { displayGraph } from './displayGraph.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1>Vite + TypeScript</h1>
-    <div>
+    <h1>Running Logs</h1>
+    <div style="position:relative;width:1300px;height:700px;">
       <canvas id="myChart"></canvas>
     </div>
   </div>
